5ae439c467621a803f729c5c597cc19fc8008d85
DavesEvilTwin1031 <daveseviltwin1031@gmail.com>
2019-01-13 00:39:28 -0500

    git test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -68,6 +68,8 @@ public class Robot extends TimedRobot {
     m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
+
+    //Dave's Evil Twin added this line in order to test get access
   }
 
   /**
